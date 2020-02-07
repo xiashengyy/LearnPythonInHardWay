c5a378d0b89b071152dc3b404fbe96f8a85dcdd4
xiashengyy <276302037@qq.com>
2020-02-07 23:23:51 +0800

    ex13

---
+++ ex13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sys import argv
+script, first, second, third = argv
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+
+# In[ ]:
+
+
+
+
