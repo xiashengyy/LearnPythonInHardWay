2b04c28204aff51d80244b6d95ddcebf09510ac4
xiashengyy <276302037@qq.com>
2020-02-08 22:15:53 +0800

    ex14~ex16

---
+++ ex15.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from sys import argv
+script, filename = argv
+txt = open(filename)
+print("Here's your file %r:" % filename)
+print(txt.read())
+print("Type the filename again:")
+file_again = input(">")
+txt_again = open(file_again)
+print(txt_again.read())
+txt_again.close()
+
